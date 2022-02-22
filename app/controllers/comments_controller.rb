843f3ec59caf9e5f15fdd680522472473d16dc14
David Pimentel <davidjohnpimentel@gmail.com>
2022-02-22 13:36:16 -0800

    Added more things

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,9 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
-        
+        redirect_to article_path(@article), status: 303
     end
+    
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body, :status)
